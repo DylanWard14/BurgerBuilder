c07a71611329641604732d12626c060412d28ab5
DylanWard14 <dylan.ward@iinet.net.au>
2019-06-17 10:18:48 +1000

    Added the build controls component in prep for functionality to dynicamically change/update the burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,25 @@ import classes from './Burger.css';
 import BurgerIngredent from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, index) => {
+            return <BurgerIngredent key={igKey + index} type={igKey}/>;
+        });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+    
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>;
+    }
+
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredent type="bread-top"/>
-            <BurgerIngredent type="cheese"/>
-            <BurgerIngredent type="meat"/>
+            {transformedIngredients}
             <BurgerIngredent type="bread-bottom"/>
         </div>
     );
