c80919db2e5904f0e02ee7845267e788115becdf
DylanWard14 <dylan.ward@iinet.net.au>
2019-06-30 22:53:56 +1000

    Create a dynamic input component to handle different forms of input, mainly created for the form elements of the order page

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,8 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders'
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
+
 class ContactData extends Component {
     state = {
         name: '',
@@ -44,10 +46,10 @@ class ContactData extends Component {
     render () {
         let form = (                
         <form>
-            <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-            <input className={classes.Input} type="email" name="email" placeholder="Your email" />
-            <input className={classes.Input} type="text" name="stree" placeholder="street" />
-            <input className={classes.Input} type="text" name="post" placeholder="Post Code" />
+            <Input inputtype="input" type="text" name="name" placeholder="Your name" />
+            <Input inputtype="input" type="email" name="email" placeholder="Your email" />
+            <Input inputtype="input" type="text" name="stree" placeholder="street" />
+            <Input inputtype="input" type="text" name="post" placeholder="Post Code" />
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>);
         if(this.state.loading)
