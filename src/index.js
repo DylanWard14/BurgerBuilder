51c94b7afcd20c46ff6e2751ed31c3d93331beb1
DylanWard14 <dylan.ward@iinet.net.au>
2019-07-14 21:45:25 +1000

    protecting information through authenication

--- src/index.js
+++ src/index.js
@@ -9,13 +9,15 @@ import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import burgerBuilderReducer from './Store/reducers/burgerBuilder';
-import orderReducer from './Store/reducers/order'
+import orderReducer from './Store/reducers/order';
+import authReducer from './Store/reducers/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
-    order: orderReducer 
+    order: orderReducer ,
+    auth: authReducer
 })
 
 
